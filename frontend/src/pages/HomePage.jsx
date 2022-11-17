68cdf1337b9368a217f7cad3fd70ca26ed74c372
HARIS#10# <mdharishz107@gmail.com>
2022-11-17 12:10:58 +0530

    updated home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import {
   Avatar,
   Box,
+  Flex,
   Image,
   Input,
   InputGroup,
@@ -10,7 +11,14 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import styles from "../Styles/HomePage.module.css";
-import { BsSearch, BsHeartFill, BsHeart } from "react-icons/bs";
+import {
+  BsSearch,
+  BsHeartFill,
+  BsHeart,
+  BsFillBookmarkFill,
+  BsBookmark,
+} from "react-icons/bs";
+import { FiMessageCircle, FiSend } from "react-icons/fi";
 import { useState } from "react";
 
 const HomePage = () => {
@@ -69,45 +77,114 @@ const HomePage = () => {
             src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
             alt={"Author"}
           />
-          <Text fontSize={'12px'} fontWeight={400}>Achim Rolle</Text>
+          <Text fontSize={"12px"} fontWeight={400}>
+            Achim Rolle
+          </Text>
         </Box>
         <Box spacing={1} align={"center"}>
           <Avatar
             src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
             alt={"Author"}
           />
-          <Text fontSize={'12px'} fontWeight={400}>Achim Rolle</Text>
+          <Text fontSize={"12px"} fontWeight={400}>
+            Achim Rolle
+          </Text>
         </Box>
         <Box spacing={1} align={"center"}>
           <Avatar
             src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
             alt={"Author"}
           />
-          <Text fontSize={'12px'} fontWeight={400}>Achim Rolle</Text>
+          <Text fontSize={"12px"} fontWeight={400}>
+            Achim Rolle
+          </Text>
         </Box>
         <Box spacing={1} align={"center"}>
           <Avatar
             src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
             alt={"Author"}
           />
-          <Text fontSize={'12px'} fontWeight={400}>Achim Rolle</Text>
+          <Text fontSize={"12px"} fontWeight={400}>
+            Achim Rolle
+          </Text>
         </Box>
         <Box spacing={1} align={"center"}>
           <Avatar
             src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
             alt={"Author"}
           />
-          <Text fontSize={'12px'} fontWeight={400}>Achim Rolle</Text>
+          <Text fontSize={"12px"} fontWeight={400}>
+            Achim Rolle
+          </Text>
         </Box>
+        
         <Box spacing={1} align={"center"}>
           <Avatar
             src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
             alt={"Author"}
           />
-          <Text fontSize={'12px'} fontWeight={400}>Achim Rolle</Text>
+          <Text fontSize={"12px"} fontWeight={400}>
+            Achim Rolle
+          </Text>
         </Box>
-       
       </div>
+
+      <div className={styles.posts}>
+        <Stack padding={"5px 10px"} direction={"row"} align={"center"}>
+          <Avatar
+            w={"40px"}
+            h={"40px"}
+            src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
+            alt={"Author"}
+          />
+          <Box>
+            <Text direction={"column"} fontSize={"12px"} fontWeight={600}>
+              Achim Rolle
+            </Text>
+          </Box>
+        </Stack>
+        <Stack>
+          <Image
+            src="https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"
+            alt=""
+          />
+        </Stack>
+
+        <Stack p={4} direction={"row"} justifyContent={'space-between'} align={"center"}>
+          <Box  display={'flex'} gap={"20px"}  >
+            <Box 
+              roundedBottom={"sm"}
+              cursor="pointer"
+              onClick={() => setLiked(!liked)}
+            >
+              {liked ? (
+                <BsHeartFill fill="red" fontSize={"22px"} />
+              ) : (
+                <BsHeart fontSize={"22px"} />
+              )}
+            </Box>
+            <Box>
+              <FiMessageCircle fontSize={"24px"} />
+            </Box>
+            <Box>
+              <FiSend fontSize={"22px"} />
+            </Box>
+          </Box>
+          <Box cursor={"pointer"} onClick={() => setLiked(!liked)}>
+            {liked ? (
+              <BsFillBookmarkFill fill="black" fontSize={"22px"} />
+            ) : (
+              <BsBookmark fontSize={"22px"} />
+            )}
+          </Box>
+        </Stack>
+      </div>
+
+      <div className={styles.bottomBox}>
+
+      </div>
+
+
     </div>
   );
 };
