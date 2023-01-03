10547b870bfa93753f4c9553af3bd603141a3f52
HARIS#10# <mdharishz107@gmail.com>
2023-01-03 23:11:38 +0530

    adding action part for auth

--- frontend/src/redux/auth/action.js
+++ frontend/src/redux/auth/action.js
@@ -1,7 +1,15 @@
-
+import * as ele from "./actionTypes";
 
 //signup
 
-export const  signup  = (payload) = (dispatch) =>{
-    // return axios
-}
+export const signup = (payload) => (dispatch) => {
+  dispatch({ type: ele.USER_SIGNUP_REQUEST });
+  return axios
+    .post("http://localhost:8000/user/signup", payload)
+    .then((res) => {
+      return dispatch({ type: ele.USER_SIGNUP_SUCCESS, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: ele.USER_SIGNUP_FAILURE, payload: err });
+    });
+};
